5710118d514e781f5135db0e6586b2964ca6b148
Sibula <valtteri.asikainen@paivola.fi>
2016-06-22 23:12:20 +0300

    Lots of unfinished stuff. Kinda get the idea.

---
+++ project/creepTiers.js
@@ -0,0 +1,6 @@
+var creepTiers = {
+// https://github.com/beije/screeps-ai/blob/master/src/CreepFactory.js
+
+}
+
+module.exports = creepTiers;
--- project/main.js
+++ project/main.js
@@ -2,6 +2,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var memorySweeper = require('memorySweeper');
+var population = require('population');
 
 module.exports.loop = function () {
 
